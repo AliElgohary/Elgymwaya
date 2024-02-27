731c05c8ef6ae5d60f3824969b09ba880f946da7
AliElgohary <alielgohary35@gmail.com>
2024-02-27 17:05:39 +0200

    nav, footer

--- adminDash/src/app/app.routes.ts
+++ adminDash/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { TrainersComponent } from './components/trainers/trainers.component';
 
 export const routes: Routes = [
   {path: '', component:HomeComponent},
-  {path: 'home', component:HomeComponent}
+  {path: 'trainers', component:TrainersComponent}
 ];
