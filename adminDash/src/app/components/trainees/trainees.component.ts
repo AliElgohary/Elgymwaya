1138c76f2b2bd205b7922d99ce0ae6798c148f17
AliElgohary <alielgohary35@gmail.com>
2024-02-28 11:50:14 +0200

    layout, Page not Found

---
+++ adminDash/src/app/components/trainees/trainees.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainees',
+  standalone: true,
+  imports: [],
+  templateUrl: './trainees.component.html',
+  styleUrl: './trainees.component.scss'
+})
+export class TraineesComponent {
+
+}
