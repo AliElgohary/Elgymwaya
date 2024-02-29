107e372dfd859770cb0a81e5a11a1dd9de115451
AliElgohary <alielgohary35@gmail.com>
2024-02-29 14:02:40 +0200

    adding Login component

--- adminDash/src/app/app.component.ts
+++ adminDash/src/app/app.component.ts
@@ -3,14 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, FooterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'adminDash';
+  isLoggedIn : boolean = false;
 }
---
+++ adminDash/src/app/components/login/login.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  user = {
+    userName: '',
+    password: '',
+  }
+  login(user : any){
+
+  }
+
+}
