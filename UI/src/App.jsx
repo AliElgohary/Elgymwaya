2c294f72f225434d0ddab350d51646ccc49eacb2
Ahmed Samy <engahmedeltonamly@gmail.com>
2024-03-18 15:13:47 +0200

    completing payment cycle

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -31,7 +31,7 @@ const routes = createBrowserRouter([
   { path: "subscriptionMonths/:id", element: <SubscriptionMonths /> },
   { path: "allTrainers", element: <AllTrainers /> },
   { path: "selectTrainer", element: <SelectTrainer /> },
-  { path: "thnx/:id", element: <Thnx /> },
+  { path: "thnx", element: <Thnx /> },
 ]);
 
 // BMI => w/h2
