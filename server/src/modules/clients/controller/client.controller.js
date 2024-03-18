5ab61915739a456532d28d6379fea93645262365
Ahmed Samy <engahmedeltonamly@gmail.com>
2024-03-18 12:31:28 +0200

    adding me route

--- server/src/modules/clients/controller/client.controller.js
+++ server/src/modules/clients/controller/client.controller.js
@@ -425,7 +425,35 @@ export const getClientById = async (req, res) => {
   try {
     const client = await userModel
       .findById(req.params.id, "-password -Cpassword")
-      .populate("coach_id", "full_name");
+      .populate({
+        path: "coach_id",
+        select: "-hiredDate -salary -client_ids",
+      })
+      .populate("plan_id");
+
+    if (!client) {
+      return res.status(404).send("User not found.");
+    }
+
+    res.json(client);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).send("Error fetching user");
+  }
+};
+
+// Get Client By Token
+export const getClientByToken = async (req, res) => {
+  try {
+    const clientID = req.userID;
+
+    const client = await userModel
+      .findById(clientID, "-password -Cpassword")
+      .populate({
+        path: "coach_id",
+        select: "-hiredDate -salary -client_ids",
+      })
+      .populate("plan_id");
 
     if (!client) {
       return res.status(404).send("User not found.");
