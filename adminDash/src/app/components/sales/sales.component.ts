246a00751e930d90135594408994a04b41aad9e0
AliElgohary <alielgohary35@gmail.com>
2024-03-22 08:59:37 +0200

    transactions in sales

--- adminDash/src/app/components/sales/sales.component.ts
+++ adminDash/src/app/components/sales/sales.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SalesService } from '../../services/sales/sales.service';
 import { ISales } from '../../models/Isales';
+import { TransactionsService } from '../../services/transactions/transactions.service';
+import { ITransaction } from '../../models/Itransactions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sales',
@@ -11,14 +14,24 @@ import { ISales } from '../../models/Isales';
 })
 export class SalesComponent implements OnInit {
   sales!: ISales;
-  constructor(private salesServ: SalesService) {}
+  transactions!: ITransaction[];
+  constructor(
+    private salesServ: SalesService,
+    private transServ: TransactionsService
+  ) {}
   ngOnInit(): void {
     this.getSales();
+    this.getTransactions();
   }
   getSales() {
     this.salesServ.getSales().subscribe((data) => {
       this.sales = data;
-      console.log(this.sales);
+    });
+  }
+  getTransactions() {
+    this.transServ.getTransactions().subscribe((data) => {
+      this.transactions = data;
+      console.log(this.transactions);
     });
   }
 }
--- adminDash/src/app/models/Itransactions.ts
+++ adminDash/src/app/models/Itransactions.ts
@@ -1,6 +1,10 @@
 export interface ITransaction {
+  _id: string;
   plan_id: string;
-  client_id: Object;
+  client_id: {
+    full_name: string;
+  };
+  payment_status: string;
   order_id: number;
   amount: number;
   subscriptionMonths: number;
