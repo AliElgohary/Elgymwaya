bf0b43bf00c3ad592169bd7ffc90f2ba92fc095c
Ahmed Samy <engahmedeltonamly@gmail.com>
2024-03-22 13:54:52 +0200

    updates

--- server/src/modules/reservation/reservation.routes.js
+++ server/src/modules/reservation/reservation.routes.js
@@ -1,10 +1,8 @@
 import express from "express";
 import { auth } from "../../middleware/auth.js";
 import {
-  cancelReservation,
   clientReservations,
   coachReservations,
-  confirmReservation,
   createReservation,
 } from "./controller/reservation.controller.js";
 import { creatSchema } from "./reservation.validation.js";
@@ -18,8 +16,7 @@ reservationRoutes.post(
   validation(creatSchema),
   createReservation
 );
-reservationRoutes.patch("/reservations/:id/confirm", auth, confirmReservation);
-reservationRoutes.patch("/reservations/:id/cancel", auth, cancelReservation);
+
 reservationRoutes.get("/clientt/reservations", auth, clientReservations);
 reservationRoutes.get("/coachh/reservations", auth, coachReservations);
 
