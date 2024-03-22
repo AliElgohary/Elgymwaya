3fb0328c86e92232f8d053b8031e1bcb42c54dd1
AliElgohary <alielgohary35@gmail.com>
2024-03-22 08:14:54 +0200

    adding sales to the backend

--- server/src/modules/mainRouter.js
+++ server/src/modules/mainRouter.js
@@ -6,6 +6,7 @@ import transactionRoutes from "./transactions/transaction.routes.js";
 import workoutPlanRoutes from "./workoutPlan/workoutPlan.routes.js";
 import analyticsRoutes from "./analytics/analytics.routes.js";
 import reservationRoutes from "./reservation/reservation.routes.js";
+import salesRouter from "./sales/sales.routes.js";
 
 const mainRouter = express.Router();
 
@@ -16,5 +17,6 @@ mainRouter.use(transactionRoutes);
 mainRouter.use(workoutPlanRoutes);
 mainRouter.use(reservationRoutes);
 mainRouter.use(analyticsRoutes);
+mainRouter.use(salesRouter);
 
 export default mainRouter;
