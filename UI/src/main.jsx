a4452193345adc1ee6ca695f276f60cee98607be
mahmoud mohamed gomaa <m.anamody10000@gmail.com>
2024-03-22 13:57:40 +0200

    update

--- UI/src/main.jsx
+++ UI/src/main.jsx
@@ -9,7 +9,6 @@ import "react-toastify/dist/ReactToastify.css";
 
 import App from "./App.jsx";
 import "./index.css";
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
