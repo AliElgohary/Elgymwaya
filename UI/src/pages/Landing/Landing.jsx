2748bb1fdd1e5947e18185989e4be11e6e83cbca
elgazar <ah152280@gmail.com>
2024-03-23 16:10:16 +0200

    change featutre sec ui

--- UI/src/pages/Landing/Landing.jsx
+++ UI/src/pages/Landing/Landing.jsx
@@ -6,30 +6,6 @@ const Landing = () => {
   return (
     <>
       <div className={styles.background}>
-        {/*<div className="d-flex align-items-center justify-content-center vh-100">
-          <div className="text-center">
-            <h1 className="mb-3">
-              WELCOME TO <span className={styles.header}>ELGYMAWEYA</span>
-            </h1>
-            <p
-              className={`display-6 md-display-6 align-self-center mb-4 ${styles.title}`}
-            >
-              "Unleash Your Potential, Powered By Fitness!"
-            </p>
-            <div className="text-center">
-              <Link to="/register">
-                <button className={`btn mx-2 ${styles.customBtn}`}>
-                  Register Now
-                </button>
-              </Link>
-              <Link to="/login">
-                <button className={`btn mx-2 ${styles.customBtn}`}>
-                  Login
-                </button>
-              </Link>
-            </div>
-          </div>
-  </div>*/}
         <Features />
       </div>
     </>
