c6fc492c29fc3af86cfe313463364a4cbef39a4d
elgazar <ah152280@gmail.com>
2024-03-24 14:16:00 +0200

    change coach details page ui

--- UI/src/pages/CoachDetails/CoachDetails.jsx
+++ UI/src/pages/CoachDetails/CoachDetails.jsx
@@ -2,8 +2,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchCoachById } from "./../../thunks/coach";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import "./CoachDetails.css";
 import { setCoach } from "./../../thunks/setCoach";
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
+import styles from './CoachDetails.module.css'
 
 const CoachDetails = () => {
   const { coachId } = useParams();
@@ -22,8 +23,8 @@ const CoachDetails = () => {
   };
 
   return (
-    <div className="container">
-      {coach && (
+    <div style={{ backgroundColor: "#101010" }}>
+      {/**coach && (
         <div className="card mt-4">
           <div className="card-body">
             <h2 className="card-title">{coach.full_name}</h2>
@@ -35,16 +36,16 @@ const CoachDetails = () => {
                   className="img-fluid rounded mb-3 h-50 w-50"
                 />
                 <p>
-                  <strong>Email:</strong> {coach.email}
+                  Email: {coach.email}
                 </p>
                 <p>
-                  <strong>Phone Number:</strong> {coach.phone_number}
+                  Phone Number: {coach.phone_number}
                 </p>
                 <p>
-                  <strong>Age:</strong> {coach.age}
+                  Age: {coach.age}
                 </p>
                 <p>
-                  <strong>Birth Date:</strong>{" "}
+                  Birth Date:{" "}
                   {new Date(coach.birth_date).toLocaleDateString()}
                 </p>
                 <h3 className="my-3 fw-bold">Working Days</h3>
@@ -52,7 +53,7 @@ const CoachDetails = () => {
                   {coach.working_days.map((day, index) => (
                     <li key={index}>
                       <p>
-                        <strong>{day.day}:</strong> {day.start_time} -{" "}
+                        {day.day}: {day.start_time} -{" "}
                         {day.end_time}
                       </p>
                     </li>
@@ -66,13 +67,13 @@ const CoachDetails = () => {
                     {coach.feedbacks.map((feedback, index) => (
                       <li key={index}>
                         <p>
-                          <strong>Rating:</strong> {feedback.rating}
+                          Rating: {feedback.rating}
                         </p>
                         <p>
-                          <strong>Comment:</strong> {feedback.comment}
+                          Comment: {feedback.comment}
                         </p>
                         <p>
-                          <strong>Date:</strong>{" "}
+                          Date:{" "}
                           {new Date(feedback.date).toLocaleDateString()}
                         </p>
                       </li>
@@ -93,7 +94,99 @@ const CoachDetails = () => {
             </button>
           </div>
         </div>
-      )}
+                )**/}
+   {coach && (
+    <section style={{ backgroundColor: "#2b2b2b" }}>
+    <MDBContainer className="py-5 h-100" style={{ backgroundColor: "#2b2b2b" }}>
+      <MDBRow className="justify-content-center align-items-center h-100">
+        <MDBCol lg="6" className="mb-4 mb-lg-0">
+          <MDBCard className="mb-3" style={{ borderRadius: '.5rem' }}>
+            <MDBRow className="g-0">
+              <MDBCol md="4" className={`gradient-custom text-center text-white ${styles.bg}`}
+                style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }  }>
+                <MDBCardImage src={coach.profile_picture}
+                  alt="Avatar" className="my-5" style={{ width: '80px' }} fluid />
+                <MDBTypography tag="h5">{coach.full_name}</MDBTypography>
+                <MDBCardText>Coach</MDBCardText>
+                <MDBIcon far icon="edit mb-5" />
+              </MDBCol>
+              <MDBCol md="8">
+                <MDBCardBody className="p-4">
+                  <MDBTypography tag="h6">Information</MDBTypography>
+                  <hr className="mt-0 mb-4" />
+                  <MDBRow className="pt-1">
+                    <MDBCol size="6" className="mb-3">
+                      <MDBTypography tag="h6">Name</MDBTypography>
+                      <MDBCardText className="text-muted">{coach.full_name}</MDBCardText>
+                      <MDBTypography tag="h6">Email</MDBTypography>
+                      <MDBCardText className="text-muted">{coach.email}</MDBCardText>
+                    </MDBCol>
+                    <MDBCol size="6" className="mb-3">
+                      <MDBTypography tag="h6">Phone</MDBTypography>
+                      <MDBCardText className="text-muted">{coach.phone_number}</MDBCardText>
+                      <MDBTypography tag="h6">Age</MDBTypography>
+                      <MDBCardText className="text-muted">{coach.age}</MDBCardText>
+                    </MDBCol>
+                    
+                  </MDBRow>
+
+                  <MDBTypography tag="h6">Work Information</MDBTypography>
+                  <hr className="mt-0 mb-4" />
+                  <MDBRow className="pt-1">
+                    <MDBCol size="6" className="mb-3">
+                      <MDBTypography tag="h6">Working Days</MDBTypography>
+                        <ul className="list-unstyled">
+                          {coach.working_days.map((day, index) => (
+                            <li key={index}>
+                              <MDBCardText className="text-muted">
+                                {day.day} {day.start_time} -{" "}{day.end_time}
+                              </MDBCardText>
+                            </li>
+                          ))}
+                        </ul>
+                    </MDBCol>
+                    <MDBCol size="12" className="mb-3">
+                      <MDBTypography tag="h6">Feedbacks</MDBTypography>
+                      {coach.feedbacks.length > 0 ? (
+                        <ul className="list-unstyled">
+                          {coach.feedbacks.map((feedback, index) => (
+                            <li key={index}>
+                              <MDBCardText>
+                                Rating: {feedback.rating}
+                              </MDBCardText>
+                              <MDBCardText>
+                                Comment: {feedback.comment}
+                              </MDBCardText>
+                              <MDBCardText>
+                                Date:{" "}
+                                {new Date(feedback.date).toLocaleDateString()}
+                              </MDBCardText>
+                            </li>
+                          ))}
+                        </ul>
+                      ) : (
+                        <p>No feedback available</p>
+                      )}
+                      <MDBCardText className="text-muted">123 456 789</MDBCardText>
+                    </MDBCol>
+                  </MDBRow>
+
+                  <div className="d-flex justify-content-start">
+                    <a href="#!"><MDBIcon fab icon="facebook me-3" size="lg" /></a>
+                    <a href="#!"><MDBIcon fab icon="twitter me-3" size="lg" /></a>
+                    <a href="#!"><MDBIcon fab icon="instagram me-3" size="lg" /></a>
+                  </div>
+                </MDBCardBody>
+              </MDBCol>
+            </MDBRow>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  </section>
+   )}
+   
+               
     </div>
   );
 };
