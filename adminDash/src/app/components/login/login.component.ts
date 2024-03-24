f46df4ce087f9024358f72e6b619e15f2a77ca0b
AliElgohary <alielgohary35@gmail.com>
2024-03-24 07:25:32 +0200

    Security and coatches populate

--- adminDash/src/app/components/login/login.component.ts
+++ adminDash/src/app/components/login/login.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -17,11 +17,13 @@ export class LoginComponent {
     email: '',
     password: '',
   };
-  constructor(private authServ: AuthService, private router:Router) {}
+  constructor(private authServ: AuthService, private router: Router) {}
   login(user: any) {
     this.authServ.login(user).subscribe(
       (res) => {
-        console.log('login susccess' + res);
+        if (res.role !== 'admin' || res.role !== 'owner') {
+          this.lodingError = true;
+        }
         this.router.navigate(['']);
       },
       (err) => {
