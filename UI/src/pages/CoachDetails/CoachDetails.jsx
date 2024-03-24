b8b8fe1595a3d4a6887bfce041783f6ec7dc6224
mahmoud mohamed gomaa <m.anamody10000@gmail.com>
2024-03-24 15:11:32 +0200

    add select coach button

--- UI/src/pages/CoachDetails/CoachDetails.jsx
+++ UI/src/pages/CoachDetails/CoachDetails.jsx
@@ -3,8 +3,18 @@ import { fetchCoachById } from "./../../thunks/coach";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { setCoach } from "./../../thunks/setCoach";
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
-import styles from './CoachDetails.module.css'
+import {
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBCard,
+  MDBCardText,
+  MDBCardBody,
+  MDBCardImage,
+  MDBTypography,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import styles from "./CoachDetails.module.css";
 
 const CoachDetails = () => {
   const { coachId } = useParams();
@@ -95,98 +105,135 @@ const CoachDetails = () => {
           </div>
         </div>
                 )**/}
-   {coach && (
-    <section style={{ backgroundColor: "#2b2b2b" }}>
-    <MDBContainer className="py-5 h-100" style={{ backgroundColor: "#2b2b2b" }}>
-      <MDBRow className="justify-content-center align-items-center h-100">
-        <MDBCol lg="6" className="mb-4 mb-lg-0">
-          <MDBCard className="mb-3" style={{ borderRadius: '.5rem' }}>
-            <MDBRow className="g-0">
-              <MDBCol md="4" className={`gradient-custom text-center text-white ${styles.bg}`}
-                style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }  }>
-                <MDBCardImage src={coach.profile_picture}
-                  alt="Avatar" className="my-5" style={{ width: '80px' }} fluid />
-                <MDBTypography tag="h5">{coach.full_name}</MDBTypography>
-                <MDBCardText>Coach</MDBCardText>
-                <MDBIcon far icon="edit mb-5" />
-              </MDBCol>
-              <MDBCol md="8">
-                <MDBCardBody className="p-4">
-                  <MDBTypography tag="h6">Information</MDBTypography>
-                  <hr className="mt-0 mb-4" />
-                  <MDBRow className="pt-1">
-                    <MDBCol size="6" className="mb-3">
-                      <MDBTypography tag="h6">Name</MDBTypography>
-                      <MDBCardText className="text-muted">{coach.full_name}</MDBCardText>
-                      <MDBTypography tag="h6">Email</MDBTypography>
-                      <MDBCardText className="text-muted">{coach.email}</MDBCardText>
+      {coach && (
+        <section style={{ backgroundColor: "#2b2b2b" }}>
+          <MDBContainer
+            className="py-5 h-100"
+            style={{ backgroundColor: "#2b2b2b" }}
+          >
+            <MDBRow className="justify-content-center align-items-center h-100">
+              <MDBCol lg="6" className="mb-4 mb-lg-0">
+                <MDBCard className="mb-3" style={{ borderRadius: ".5rem" }}>
+                  <MDBRow className="g-0">
+                    <MDBCol
+                      md="4"
+                      className={`gradient-custom text-center text-white ${styles.bg}`}
+                      style={{
+                        borderTopLeftRadius: ".5rem",
+                        borderBottomLeftRadius: ".5rem",
+                      }}
+                    >
+                      <MDBCardImage
+                        src={coach.profile_picture}
+                        alt="Avatar"
+                        className="my-5"
+                        style={{ width: "80px" }}
+                        fluid
+                      />
+                      <MDBTypography tag="h5">{coach.full_name}</MDBTypography>
+                      <MDBCardText>Coach</MDBCardText>
+                      <MDBIcon far icon="edit mb-5" />
                     </MDBCol>
-                    <MDBCol size="6" className="mb-3">
-                      <MDBTypography tag="h6">Phone</MDBTypography>
-                      <MDBCardText className="text-muted">{coach.phone_number}</MDBCardText>
-                      <MDBTypography tag="h6">Age</MDBTypography>
-                      <MDBCardText className="text-muted">{coach.age}</MDBCardText>
-                    </MDBCol>
-                    
-                  </MDBRow>
+                    <MDBCol md="8">
+                      <MDBCardBody className="p-4">
+                        <MDBTypography tag="h6">Information</MDBTypography>
+                        <hr className="mt-0 mb-4" />
+                        <MDBRow className="pt-1">
+                          <MDBCol size="6" className="mb-3">
+                            <MDBTypography tag="h6">Name</MDBTypography>
+                            <MDBCardText className="text-muted">
+                              {coach.full_name}
+                            </MDBCardText>
+                            <MDBTypography tag="h6">Email</MDBTypography>
+                            <MDBCardText className="text-muted">
+                              {coach.email}
+                            </MDBCardText>
+                          </MDBCol>
+                          <MDBCol size="6" className="mb-3">
+                            <MDBTypography tag="h6">Phone</MDBTypography>
+                            <MDBCardText className="text-muted">
+                              {coach.phone_number}
+                            </MDBCardText>
+                            <MDBTypography tag="h6">Age</MDBTypography>
+                            <MDBCardText className="text-muted">
+                              {coach.age}
+                            </MDBCardText>
+                          </MDBCol>
+                        </MDBRow>
 
-                  <MDBTypography tag="h6">Work Information</MDBTypography>
-                  <hr className="mt-0 mb-4" />
-                  <MDBRow className="pt-1">
-                    <MDBCol size="6" className="mb-3">
-                      <MDBTypography tag="h6">Working Days</MDBTypography>
-                        <ul className="list-unstyled">
-                          {coach.working_days.map((day, index) => (
-                            <li key={index}>
-                              <MDBCardText className="text-muted">
-                                {day.day} {day.start_time} -{" "}{day.end_time}
-                              </MDBCardText>
-                            </li>
-                          ))}
-                        </ul>
-                    </MDBCol>
-                    <MDBCol size="12" className="mb-3">
-                      <MDBTypography tag="h6">Feedbacks</MDBTypography>
-                      {coach.feedbacks.length > 0 ? (
-                        <ul className="list-unstyled">
-                          {coach.feedbacks.map((feedback, index) => (
-                            <li key={index}>
-                              <MDBCardText>
-                                Rating: {feedback.rating}
-                              </MDBCardText>
-                              <MDBCardText>
-                                Comment: {feedback.comment}
-                              </MDBCardText>
-                              <MDBCardText>
-                                Date:{" "}
-                                {new Date(feedback.date).toLocaleDateString()}
-                              </MDBCardText>
-                            </li>
-                          ))}
-                        </ul>
-                      ) : (
-                        <p>No feedback available</p>
-                      )}
-                      <MDBCardText className="text-muted">123 456 789</MDBCardText>
+                        <MDBTypography tag="h6">Work Information</MDBTypography>
+                        <hr className="mt-0 mb-4" />
+                        <MDBRow className="pt-1">
+                          <MDBCol size="6" className="mb-3">
+                            <MDBTypography tag="h6">Working Days</MDBTypography>
+                            <ul className="list-unstyled">
+                              {coach.working_days.map((day, index) => (
+                                <li key={index}>
+                                  <MDBCardText className="text-muted">
+                                    {day.day} {day.start_time} - {day.end_time}
+                                  </MDBCardText>
+                                </li>
+                              ))}
+                            </ul>
+                          </MDBCol>
+                          <MDBCol size="12" className="mb-3">
+                            <MDBTypography tag="h6">Feedbacks</MDBTypography>
+                            {coach.feedbacks.length > 0 ? (
+                              <ul className="list-unstyled">
+                                {coach.feedbacks.map((feedback, index) => (
+                                  <li key={index}>
+                                    <MDBCardText>
+                                      Rating: {feedback.rating}
+                                    </MDBCardText>
+                                    <MDBCardText>
+                                      Comment: {feedback.comment}
+                                    </MDBCardText>
+                                    <MDBCardText>
+                                      Date:{" "}
+                                      {new Date(
+                                        feedback.date
+                                      ).toLocaleDateString()}
+                                    </MDBCardText>
+                                  </li>
+                                ))}
+                              </ul>
+                            ) : (
+                              <p>No feedback available</p>
+                            )}
+                            <MDBCardText className="text-muted">
+                              123 456 789
+                            </MDBCardText>
+                          </MDBCol>
+                        </MDBRow>
+
+                        <div className="d-flex justify-content-start">
+                          <a href="#!">
+                            <MDBIcon fab icon="facebook me-3" size="lg" />
+                          </a>
+                          <a href="#!">
+                            <MDBIcon fab icon="twitter me-3" size="lg" />
+                          </a>
+                          <a href="#!">
+                            <MDBIcon fab icon="instagram me-3" size="lg" />
+                          </a>
+                        </div>
+                        <div className="text-center mb-3">
+                          <button
+                            className="btn btn-primary btn-lg"
+                            onClick={handleSelectedCoach}
+                          >
+                            select
+                          </button>
+                        </div>
+                      </MDBCardBody>
                     </MDBCol>
                   </MDBRow>
-
-                  <div className="d-flex justify-content-start">
-                    <a href="#!"><MDBIcon fab icon="facebook me-3" size="lg" /></a>
-                    <a href="#!"><MDBIcon fab icon="twitter me-3" size="lg" /></a>
-                    <a href="#!"><MDBIcon fab icon="instagram me-3" size="lg" /></a>
-                  </div>
-                </MDBCardBody>
+                </MDBCard>
               </MDBCol>
             </MDBRow>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  </section>
-   )}
-   
-               
+          </MDBContainer>
+        </section>
+      )}
     </div>
   );
 };
--- UI/src/pages/CoachHome/WorkoutForm.jsx
+++ UI/src/pages/CoachHome/WorkoutForm.jsx
@@ -1,3 +1,433 @@
+// import { useState } from "react";
+// import { Form, Button, Row, Col } from "react-bootstrap";
+// import styles from "./TraineeRotine.module.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPlus } from "@fortawesome/free-solid-svg-icons";
+// import { crateRoutineInit } from "../../thunks/users";
+// import { useDispatch } from "react-redux";
+// import Input from "./../../components/common/Input";
+// import Joi from "joi";
+// // eslint-disable-next-line react/prop-types
+// const WorkoutForm = ({ clientId, handleCloseModal }) => {
+//   const dispatch = useDispatch();
+
+//   // Joi schema for form validation
+//   const schema = Joi.object({
+//     startDate: Joi.date().required(),
+//     endDate: Joi.date().required(),
+//     status: Joi.string().valid("Active", "Completed", "Paused").required(),
+//     goals: Joi.string().required(),
+//     notes: Joi.string().required(),
+//     workouts: Joi.array()
+//       .items(
+//         Joi.object({
+//           name: Joi.string().required(),
+//           description: Joi.number().required(),
+//           frequency: Joi.string().required(),
+//           duration: Joi.number().required(),
+//           type: Joi.string().required(),
+//           sets: Joi.number().required(),
+//           reps: Joi.number().required(),
+//           rest_period: Joi.number().required(),
+//           equipment_needed: Joi.string().required(),
+//           video_link: Joi.string().uri().required(),
+//         })
+//       )
+//       .required(), // Ensure that the workouts array is required
+//   });
+
+//   const [formData, setFormData] = useState({
+//     startDate: "",
+//     endDate: "",
+//     status: "Active",
+//     goals: "",
+//     notes: "",
+//     workouts: [
+//       {
+//         name: "",
+//         description: "",
+//         frequency: "",
+//         duration: "",
+//         type: "",
+//         sets: "",
+//         reps: "",
+//         rest_period: "",
+//         equipment_needed: "",
+//         video_link: "",
+//       },
+//     ],
+//   });
+
+//   const [errors, setErrors] = useState({});
+
+//   const validateForm = () => {
+//     const { error } = schema.validate(formData, { abortEarly: false });
+//     if (!error) return {};
+
+//     const validationErrors = {};
+//     for (let item of error.details) {
+//       if (item.path[0] === "workouts") {
+//         if (!validationErrors.workouts) {
+//           validationErrors.workouts = [];
+//         }
+//         validationErrors.workouts[item.path[1]] = item.message;
+//       } else {
+//         validationErrors[item.path[0]] = item.message;
+//       }
+//     }
+//     return validationErrors;
+//   };
+
+//   const handleWorkoutChange = (index, e) => {
+//     const { name, value } = e.target;
+//     const updatedWorkouts = [...formData.workouts];
+//     updatedWorkouts[index][name] = value;
+//     setFormData({ ...formData, workouts: updatedWorkouts });
+//   };
+
+//   const addWorkout = () => {
+//     setFormData({
+//       ...formData,
+//       workouts: [
+//         ...formData.workouts,
+//         {
+//           name: "",
+//           description: "",
+//           frequency: "",
+//           duration: "",
+//           type: "",
+//           sets: "",
+//           reps: "",
+//           rest_period: "",
+//           equipment_needed: "",
+//           video_link: "",
+//         },
+//       ],
+//     });
+//   };
+
+//   const removeWorkout = (index) => {
+//     const updatedWorkouts = [...formData.workouts];
+//     updatedWorkouts.splice(index, 1);
+//     setFormData({ ...formData, workouts: updatedWorkouts });
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     const validationErrors = validateForm();
+//     if (Object.keys(validationErrors).length > 0) {
+//       setErrors(validationErrors);
+//       return;
+//     }
+
+//     formData["client_id"] = clientId;
+//     dispatch(crateRoutineInit(formData));
+//     handleCloseModal();
+//   };
+
+//   return (
+//     <Form onSubmit={handleSubmit} className={styles.modal_bg}>
+//       <Form.Group controlId="startDate">
+//         <Form.Label className="pt-1 mb-1">Start Date</Form.Label>
+//         <Input
+//           className={styles.input_modal}
+//           type="date"
+//           name="startDate"
+//           value={formData.startDate}
+//           onChange={(e) =>
+//             setFormData({ ...formData, startDate: e.target.value })
+//           }
+//         />
+//         {errors.startDate && (
+//           <span className="text-danger">{errors.startDate}</span>
+//         )}
+//       </Form.Group>
+
+//       <Form.Group controlId="endDate">
+//         <Form.Label className="pt-4 mb-1">End Date</Form.Label>
+//         <Input
+//           className={styles.input_modal}
+//           type="date"
+//           name="endDate"
+//           value={formData.endDate}
+//           onChange={(e) =>
+//             setFormData({ ...formData, endDate: e.target.value })
+//           }
+//         />
+//         {errors.endDate && (
+//           <span className="text-danger">{errors.endDate}</span>
+//         )}
+//       </Form.Group>
+
+//       <Form.Group controlId="status">
+//         <Form.Label className="pt-4 mb-1">Status</Form.Label>
+//         <Form.Control
+//           as="select"
+//           name="status"
+//           value={formData.status}
+//           onChange={(e) => setFormData({ ...formData, status: e.target.value })}
+//         >
+//           <option value="active">Active</option>
+//           <option value="completed">Completed</option>
+//           <option value="paused">Paused</option>
+//         </Form.Control>
+//       </Form.Group>
+
+//       {/* Workouts */}
+//       <h4 className={styles.workoutsFormTitle}>Workouts</h4>
+//       {formData.workouts.map((workout, index) => (
+//         <div className={styles.workoutFormContainer} key={index}>
+//           <h5>Workout {index + 1}</h5>
+//           <Row>
+//             <Col>
+//               <Form.Group controlId={`workoutName-${index}`}>
+//                 <Form.Label className="pt-4 mb-1">Name</Form.Label>
+//                 <Input
+//                   className={styles.input_modal}
+//                   type="text"
+//                   name="name"
+//                   value={workout.name}
+//                   onChange={(e) => handleWorkoutChange(index, e)}
+//                 />
+//                 {errors.workouts &&
+//                   errors.workouts[index] &&
+//                   errors.workouts[index].name && (
+//                     <span className="text-danger">
+//                       {errors.workouts[index].name}
+//                     </span>
+//                   )}
+//               </Form.Group>
+//             </Col>
+//             <Col>
+//               <Form.Group controlId={`workoutDescription-${index}`}>
+//                 <Form.Label className="pt-4 mb-1">Description</Form.Label>
+//                 <Input
+//                   className={styles.input_modal}
+//                   type="text"
+//                   name="description"
+//                   value={workout.description}
+//                   onChange={(e) => handleWorkoutChange(index, e)}
+//                 />
+//                 {errors.workouts &&
+//                   errors.workouts[index] &&
+//                   errors.workouts[index].description && (
+//                     <span className="text-danger">
+//                       {errors.workouts[index].description}
+//                     </span>
+//                   )}
+//               </Form.Group>
+//             </Col>
+//           </Row>
+//           <Row>
+//             <Col>
+//               <Form.Group controlId={`workoutFrequency-${index}`}>
+//                 <Form.Label className="pt-4 mb-1">Frequency</Form.Label>
+//                 <Input
+//                   className={styles.input_modal}
+//                   type="text"
+//                   name="frequency"
+//                   value={workout.frequency}
+//                   onChange={(e) => handleWorkoutChange(index, e)}
+//                 />
+//                 {errors.workouts &&
+//                   errors.workouts[index] &&
+//                   errors.workouts[index].frequency && (
+//                     <span className="text-danger">
+//                       {errors.workouts[index].frequency}
+//                     </span>
+//                   )}
+//               </Form.Group>
+//             </Col>
+//             <Col>
+//               <Form.Group controlId={`workoutDuration-${index}`}>
+//                 <Form.Label className="pt-4 mb-1">
+//                   Duration (minutes)
+//                 </Form.Label>
+//                 <Input
+//                   className={styles.input_modal}
+//                   type="number"
+//                   name="duration"
+//                   value={workout.duration}
+//                   onChange={(e) => handleWorkoutChange(index, e)}
+//                 />
+//                 {errors.workouts &&
+//                   errors.workouts[index] &&
+//                   errors.workouts[index].duration && (
+//                     <span className="text-danger">
+//                       {errors.workouts[index].duration}
+//                     </span>
+//                   )}
+//               </Form.Group>
+//             </Col>
+//           </Row>
+//           <Row>
+//             <Col>
+//               <Form.Group controlId={`workoutType-${index}`}>
+//                 <Form.Label className="pt-4 mb-1">Type</Form.Label>
+//                 <Input
+//                   className={styles.input_modal}
+//                   type="text"
+//                   name="type"
+//                   value={workout.type}
+//                   onChange={(e) => handleWorkoutChange(index, e)}
+//                 />
+//                 {errors.workouts &&
+//                   errors.workouts[index] &&
+//                   errors.workouts[index].type && (
+//                     <span className="text-danger">
+//                       {errors.workouts[index].type}
+//                     </span>
+//                   )}
+//               </Form.Group>
+//             </Col>
+//             <Col>
+//               <Form.Group controlId={`workoutSets-${index}`}>
+//                 <Form.Label className="pt-4 mb-1">Sets</Form.Label>
+//                 <Input
+//                   className={styles.input_modal}
+//                   type="number"
+//                   name="sets"
+//                   value={workout.sets}
+//                   onChange={(e) => handleWorkoutChange(index, e)}
+//                 />
+//                 {errors.workouts &&
+//                   errors.workouts[index] &&
+//                   errors.workouts[index].sets && (
+//                     <span className="text-danger">
+//                       {errors.workouts[index].sets}
+//                     </span>
+//                   )}
+//               </Form.Group>
+//             </Col>
+//           </Row>
+//           <Row>
+//             <Col>
+//               <Form.Group controlId={`workoutReps-${index}`}>
+//                 <Form.Label className="pt-4 mb-1">Reps</Form.Label>
+//                 <Input
+//                   className={styles.input_modal}
+//                   type="number"
+//                   name="reps"
+//                   value={workout.reps}
+//                   onChange={(e) => handleWorkoutChange(index, e)}
+//                 />
+//                 {errors.workouts &&
+//                   errors.workouts[index] &&
+//                   errors.workouts[index].reps && (
+//                     <span className="text-danger">
+//                       {errors.workouts[index].reps}
+//                     </span>
+//                   )}
+//               </Form.Group>
+//             </Col>
+//             <Col>
+//               <Form.Group controlId={`workoutRestPeriod-${index}`}>
+//                 <Form.Label className="pt-4 mb-1">
+//                   Rest Period (seconds)
+//                 </Form.Label>
+//                 <Input
+//                   className={styles.input_modal}
+//                   type="number"
+//                   name="rest_period"
+//                   value={workout.rest_period}
+//                   onChange={(e) => handleWorkoutChange(index, e)}
+//                 />
+//                 {errors.workouts &&
+//                   errors.workouts[index] &&
+//                   errors.workouts[index].rest_period && (
+//                     <span className="text-danger">
+//                       {errors.workouts[index].rest_period}
+//                     </span>
+//                   )}
+//               </Form.Group>
+//             </Col>
+//           </Row>
+//           <Form.Group controlId={`workoutEquipment-${index}`}>
+//             <Form.Label className="pt-4 mb-1">Equipment Needed</Form.Label>
+//             <Input
+//               className={styles.input_modal}
+//               type="text"
+//               name="equipment_needed"
+//               value={workout.equipment_needed}
+//               onChange={(e) => handleWorkoutChange(index, e)}
+//             />
+//             {errors.workouts &&
+//               errors.workouts[index] &&
+//               errors.workouts[index].equipment_needed && (
+//                 <span className="text-danger">
+//                   {errors.workouts[index].equipment_needed}
+//                 </span>
+//               )}
+//           </Form.Group>
+//           <Form.Group controlId={`workoutVideoLink-${index}`}>
+//             <Form.Label className="pt-4 mb-1">Video Link</Form.Label>
+//             <Input
+//               className={styles.input_modal}
+//               type="text"
+//               name="video_link"
+//               value={workout.video_link}
+//               onChange={(e) => handleWorkoutChange(index, e)}
+//             />
+//             {errors.workouts &&
+//               errors.workouts[index] &&
+//               errors.workouts[index].video_link && (
+//                 <span className="text-danger">
+//                   {errors.workouts[index].video_link}
+//                 </span>
+//               )}
+//           </Form.Group>
+//           <Button
+//             className={styles.removeWorkout}
+//             variant="danger"
+//             onClick={() => removeWorkout(index)}
+//           >
+//             Remove Workout
+//           </Button>
+//         </div>
+//       ))}
+//       <Button
+//         variant="primary"
+//         className={styles.addWorkout}
+//         onClick={addWorkout}
+//       >
+//         <FontAwesomeIcon icon={faPlus} />
+//         Add
+//       </Button>
+
+//       <Form.Group controlId="goals">
+//         <Form.Label className="pt-4 mb-1">Goals</Form.Label>
+//         <Input
+//           className={styles.input_modal}
+//           as="textarea"
+//           rows={3}
+//           name="goals"
+//           value={formData.goals}
+//           onChange={(e) => setFormData({ ...formData, goals: e.target.value })}
+//         />
+//         {errors.goals && <span className="text-danger">{errors.goals}</span>}
+//       </Form.Group>
+
+//       <Form.Group controlId="notes">
+//         <Form.Label className="pt-4 mb-1">Notes</Form.Label>
+//         <Input
+//           className={styles.input_modal}
+//           as="textarea"
+//           rows={3}
+//           name="notes"
+//           value={formData.notes}
+//           onChange={(e) => setFormData({ ...formData, notes: e.target.value })}
+//         />
+//         {errors.notes && <span className="text-danger">{errors.notes}</span>}
+//       </Form.Group>
+
+//       <Button variant="primary" type="submit">
+//         Submit
+//       </Button>
+//     </Form>
+//   );
+// };
+
+// export default WorkoutForm;
 import { useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import styles from "./TraineeRotine.module.css";
